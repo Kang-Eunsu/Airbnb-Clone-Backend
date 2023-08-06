aa5596a6ff4ce9709dca6e8561724a201be19585
Kang-Eunsu <kanges802@g.skku.edu>
2023-08-06 13:18:05 +0900

    Clean views.py by using ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -5,54 +5,9 @@ from rest_framework.status import HTTP_202_ACCEPTED
 from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework.viewsets import ModelViewSet
 
 
-class Categories(APIView):
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response(
-            {
-                "ok": True,
-                "categories": serializer.data,
-            }
-        )
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(
-                CategorySerializer(new_category).data,
-            )
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except:
-            raise NotFound
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_202_ACCEPTED)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
